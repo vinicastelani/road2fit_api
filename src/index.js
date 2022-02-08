c71d8fa6a96d914faf10049f2e9bf09cfd4be31c
Vinícius Castelani dos Santos <visacastelani@gmail.com>
2022-02-07 22:25:53 -0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const cors = require('cors')
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
 const app = express();
-
+app.use(cors())
 app.use(bodyParser.json());
 app.use(
     bodyParser.urlencoded({
