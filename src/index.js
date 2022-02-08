cafe2ab516e3610c7692a64f8ef5a8025218b0a3
Vinícius Castelani dos Santos <visacastelani@gmail.com>
2022-02-07 22:43:27 -0300

    fix

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
-app.use(cors())
 app.use(bodyParser.json());
 app.use(
     bodyParser.urlencoded({
@@ -15,6 +14,7 @@ app.use(
         parameterLimit: 50000,
     })
 );
+app.use(cors())
 
 require('./controllers/AuthController')(app)
 
